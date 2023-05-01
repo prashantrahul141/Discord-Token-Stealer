5210c58091d4d7c11bca11346b931b65a09b5e20
Prashant <prashantrahul141@protonmail.com>
2023-05-01 22:12:09 +0530

    add:  logging

--- Program.py
+++ Program.py
@@ -1,22 +1,46 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
+from logger import logger
+try:
+    from selenium.webdriver.common.by import By
+    from selenium import webdriver
+    logger.debug("imported selenium.")
+except:
+    logger.error("could not import selenium, exiting.")
+    exit()
+
 from time import sleep
-from constants import LOGIN_DISCORD_URL, QR_SVG_ELEMENT_SELECTOR, IMAGE_LOCATION
+from constants import LOGIN_DISCORD_URL, QR_ELEMENT_SELECTOR
 
 
 class Program:
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        logger.debug("Starting firefox.")
+        try:
+            self.driver = webdriver.Firefox()
+        except:
+            logger.error("could not start firefox, exiting.")
+            exit()
 
     def start(self):
+        logger.debug("opening discord log in page.")
         self.driver.get(LOGIN_DISCORD_URL)
-        self.getQRCodeElement()
 
-    def getQRCodeElement(self):
+    def getQRCodeElementAndSave(self, screenshotPath: str) -> bool:
+        logger.info("sleeping 5 seconds for qr image to load.")
         sleep(5)
-        self.svg_element = self.driver.find_element(
-            By.CSS_SELECTOR, QR_SVG_ELEMENT_SELECTOR)
-        self.saveQR()
+        logger.debug("searching qr html element")
+        try:
+            self.qr_element = self.driver.find_element(
+                By.CSS_SELECTOR, QR_ELEMENT_SELECTOR)
+            logger.info("found qr element")
+        except:
+            logger.error("could not find qr html element on the page, exiting.")
+            exit()
 
-    def saveQR(self):
-        self.svg_element.screenshot(IMAGE_LOCATION)
+        logger.info("saving qr element as png")
+        try:
+            result = self.qr_element.screenshot(screenshotPath)
+            logger.info(f"saved qr element as png at '{screenshotPath}'")
+            return result
+        except:
+            logger.error("could not save image, exiting.")
+            exit()
