29cbce5a4dc002f51f8681545809b89afafc0f1b
Prashant <prashantrahul141@protonmail.com>
2023-05-02 14:16:19 +0530

    using client instead of bot

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from Program import Program
-
-
 def main():
-    program = Program()
-    program.start()
+    from os import getenv
+    from logger import logger
+    logger.debug("loading env.")
+    from dotenv import load_dotenv
+    load_dotenv()
+    from Bot import Client
+    from discord import Intents
+    intents = Intents.default()
+    intents.message_content = True
+    client = Client(intents=intents)
+    client.run(getenv("DISCORD_BOT_TOKEN"))
 
 
 if __name__ == '__main__':
