f3acc434b605a2a1f087ea06a644a565654dd51b
Prashant <prashantrahul141@protonmail.com>
2023-05-03 14:21:51 +0530

    add: more logging

--- Bot.py
+++ Bot.py
@@ -1,7 +1,7 @@
 import discord
 from logger import logger
 from SeleniumDriver import SeleniumDriver
-from Utils import verifyToken, saveToken
+from Utils import verifyToken, saveToken, readTokens
 
 
 class Client(discord.Client):
@@ -21,13 +21,17 @@ class Client(discord.Client):
             embed = discord.Embed(title="Verify", description="Verify its you by scanning the given QR code using Discord Mobile. Then click on 'Yes' to verify its you.",
                                   colour=discord.Color.blue())
             embed.set_image(url="attachment://qr.png")
-
             logger.info("sending embed.")
             await message.author.send(file=file, embed=embed)
             await warning_message.edit(content="Verification code sent, Check your dms.")
+
             if self.driver.waitForScan():
                 if self.driver.waitForLogin():
                     logger.info("got token")
                     token = self.driver.getToken()
+
                     if verifyToken(token):
+                        logger.info("verified token")
                         saveToken(token)
+                    else:
+                        logger.info("false token.")
