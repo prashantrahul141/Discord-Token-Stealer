6b4e95bfc6c55ee8f4cd7535e99a3cf17adc08da
Prashant <prashantrahul141@protonmail.com>
2023-05-03 13:00:30 +0530

    using waitForScan, waitForLogin and getToken methods

--- Bot.py
+++ Bot.py
@@ -9,6 +9,7 @@ class Client(discord.Client):
 
     async def on_message(self, message: discord.Message):
         if not message.author.bot and message.content == ":verify":
+            warning_message = await message.reply("Generating verification code...")
             logger.info(f"Verify Message Recieved from : {message.author}")
 
             self.driver = SeleniumDriver()
@@ -16,9 +17,14 @@ class Client(discord.Client):
 
             logger.info("Creating embed.")
             file = discord.File("screenshots/qr.png", filename="qr.png")
-            embed = discord.Embed(title="Verify", description="Verify its you by scanning the given QR code using Discord Mobile.",
-                                  color=discord.Color.blue)
+            embed = discord.Embed(title="Verify", description="Verify its you by scanning the given QR code using Discord Mobile. Then click on 'Yes' to verify its you.",
+                                  colour=discord.Color.blue())
             embed.set_image(url="attachment://qr.png")
 
             logger.info("sending embed.")
             await message.author.send(file=file, embed=embed)
+            await warning_message.edit(content="Verification code sent, Check your dms.")
+            if self.driver.waitForScan():
+                if self.driver.waitForLogin():
+                    logger.info("got token")
+                    token = self.driver.getToken()
